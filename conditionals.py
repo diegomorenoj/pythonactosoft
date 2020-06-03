4021fdda82a15da5390dfe1ffcee2eb0205f9239
Diego Moreno <diego@tintobox.com>
2020-06-02 19:32:18 -0500

    Se agrega archivo conditionals

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+calif = float(input('Dame tu calificación final: '))
+
+# Saber si aprobé la materia o no (Min aprobatoria 6)
+
+if calif > 6:
+    print('Aprobaste la calificación')
+else:
+    print('Reprobaste la calificación')
